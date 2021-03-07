838318871f8052cc33a5cc5c0851fe38ecdec1df
Subhajit Sahu <wolfram77@gmail.com>
2021-03-07 11:43:27 +0530

    :ringed_planet: modernize build, index

--- build.js
+++ build.js
@@ -4,14 +4,25 @@ const os = require('os');
 
 var map = new Map();
 var stream = fs.createReadStream('index.csv').pipe(csv.parse({columns: true, comment: '#'}));
-stream.on('data', (r) => {
+
+
+
+
+function writeFile(pth, d) {
+  d = d.replace(/\r?\n/g, os.EOL);
+  fs.writeFileSync(pth, d);
+}
+
+
+stream.on('data', r => {
   map.set(r.abbr.replace(/\W/g, '').toLowerCase(), {abbr: r.abbr, lang: r.lang});
 });
+
 stream.on('end', () => {
-  var z = `const CORPUS = new Map([${os.EOL}`;
-  for(var [k, v] of map)
-    z += `  ["${k}", ${JSON.stringify(v).replace(/\"(\w+)\":/g, '$1:')}],${os.EOL}`;
-  z += `]);${os.EOL}`;
-  z += `module.exports = CORPUS;${os.EOL}`;
-  fs.writeFileSync('corpus.js', z);
+  var a = `const CORPUS = new Map([\n`;
+  for (var [k, v] of map)
+    a += `  ["${k}", ${JSON.stringify(v).replace(/\"(\w+)\":/g, '$1:')}],\n`;
+  a += `]);\n`;
+  a += `module.exports = CORPUS;\n`;
+  writeFile('corpus.js', a);
 });
--- index.js
+++ index.js
@@ -1,53 +1,50 @@
-const Sql = require('sql-extra');
 const path = require('path');
+const esql = require('sql-extra');
 
 const REPLACE = /((\w\s+|\w\.\s*|\w\-\s*|\w$)+)|\w+/g;
-var corpus = new Map();
+var corpus = null;
 var match = null;
-var ready = false;
+
+
 
 
 function createRegex(lst) {
-  var z = '(^|\\W+)(';
-  for(var v of lst)
-    z += v.length>1? `${v}|`:`${v}\\.|`;
-  z = z.substring(0, z.length-1);
-  z += `)(\\W+|$)`;
-  return new RegExp(z, 'i');
+  var a = '(^|\\W+)(';
+  for (var v of lst)
+    a += v.length>1? `${v}|`:`${v}\\.|`;
+  a = a.substring(0, a.length-1);
+  a += `)(\\W+|$)`;
+  return new RegExp(a, 'i');
 }
 
-function loadCorpus() {
-  for(var [k, v] of require('./corpus'))
-    corpus.set(k, v);
-};
+function load() {
+  if (corpus) return corpus;
+  corpus = require('./corpus');
+  match = createRegex(corpus.keys());
+  return corpus;
+}
 
 function csv() {
   return path.join(__dirname, 'index.csv');
-};
+}
 
 function sql(tab='languages', opt={}) {
-  return Sql.setupTable(tab, {abbr: 'TEXT', lang: 'TEXT'}, require('./corpus').values(),
+  return esql.setupTable(tab, {abbr: 'TEXT', lang: 'TEXT'}, require('./corpus').values(),
     Object.assign({pk: 'abbr', index: true, tsvector: {abbr: 'A', lang: 'B'}}, opt));
-};
+}
 
-function load() {
-  if(ready) return true;
-  loadCorpus(); match = createRegex(corpus.keys());
-  return ready = true;
-};
 
 function languages(txt) {
-  if(match==null) return null;
-  var txt = txt.replace(REPLACE, (m, p1) => {
+  if (!corpus) load();
+  var txt = txt.replace(REPLACE, m => {
     var v = m.replace(/\W/g, '');
-    return v.length===1? `${m.trim()} `:`${v} `
+    return v.length===1? m.trim()+' ' : v+' ';
   }).toLowerCase();
   var m = txt.match(match);
-  if(m==null) return null;
+  if (m==null) return null;
   return corpus.get(m[2].replace('.', ''));
-};
+}
+languages.load = load;
 languages.csv = csv;
 languages.sql = sql;
-languages.load = load;
-languages.corpus = corpus;
 module.exports = languages;
