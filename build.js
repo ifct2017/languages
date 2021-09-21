1b4517ca46f80de7bfc779820dfa1d5367b3af78
Subhajit Sahu <wolfram77@gmail.com>
2021-09-21 17:50:29 +0530

    :skunk: fix mixme security vulnerability

--- build.js
+++ build.js
@@ -1,9 +1,9 @@
-const csv = require('csv');
+const csvParse = require('csv-parse');
 const fs = require('fs');
 const os = require('os');
 
 var map = new Map();
-var stream = fs.createReadStream('index.csv').pipe(csv.parse({columns: true, comment: '#'}));
+var stream = fs.createReadStream('index.csv').pipe(csvParse({columns: true, comment: '#'}));
 
 
 
